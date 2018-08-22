3c58e2f442caecb47abb2276390e63356c6cdba0
terenitup <terentaniuchi@gmail.com>
2018-08-21 20:30:17 -0600

    added header and header styles.

--- madlibs2/src/components/app.js
+++ madlibs2/src/components/app.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+      <div className='backgroundSkew'>
+        <div className="madlib-heading">
+            <h1>Bottega Mad Libs</h1>  
+            <div className="madlib-subheading">
+              Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
+            </div>
+        </div>
       </div>
     );
   }

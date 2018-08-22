1935af64fee6b5a1160567fc0c570764e88600f5
terenitup <terentaniuchi@gmail.com>
2018-08-22 16:25:16 -0600

    created a css grid for card and inputs

--- madlibs2/src/components/content.js
+++ madlibs2/src/components/content.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Content extends Component {
   render() {
     const data = this.props.data;
-    this.props.data.map((object,index) => {
-        console.log(index);
-    })
+    // this.props.data.map((object,index) => {
+    //     console.log(index);
+    // })
     // <span>{data.indexOf(data.color)}</span>
         return (
             <p>

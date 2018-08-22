eb167d6b456c53499bf2eaf836ea3a6b711f04ed
terenitup <terentaniuchi@gmail.com>
2018-08-22 17:44:09 -0600

    set up remaining grids

--- madlibs2/src/components/home.js
+++ madlibs2/src/components/home.js
@@ -9,12 +9,12 @@ export default class Home extends Component {
   render() {
     return (
       <div className='backgroundSkew'>
-        <div className="madlib-heading">
-            <h1>Bottega Mad Libs</h1>  
+        <div className="home">
+            {/* <h1>Bottega Mad Libs</h1>  
             <div className="madlib-subheading">
               Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
             </div>
-            {/* <Madlib_form /> */}
+            <Madlib_form /> */}
             {Header()}
             <Card />
         </div>
--- madlibs2/src/components/input.js
+++ madlibs2/src/components/input.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Input = ({title, state, name}, onChange, index) => {
     return (
-        <div key={index +1} className="input">
-        <label className="input__number">{index}</label>
+        <div key={index + 1} className="input">
+        <label className="input__number">{index + 1}</label>
             <input value={state} onChange={onChange} name={name}/>
             <label className="input__title">{title}</label>
         </div>

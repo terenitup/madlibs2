970639d1581baaacc9d47555615cee703bbab2ee
terenitup <terentaniuchi@gmail.com>
2018-08-22 11:25:07 -0600

    add remaining inputs

--- madlibs2/src/components/input.js
+++ madlibs2/src/components/input.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Input = (title, state, onChange, name) => {
+const Input = ({title, state, name}, onChange) => {
     return (
-        <div className="input">
+        <div key={name} className="input">
             <input  value={state} onChange={onChange} name={name}/>
             <label>{title}</label>
         </div>

8862fb92b366270a14b03ca9d75b26ef47c6255b
terenitup <terentaniuchi@gmail.com>
2018-09-13 21:48:11 -0600

    edited the numbers so they change colors from gray to green as string is entered

--- madlibs2/src/components/input.js
+++ madlibs2/src/components/input.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Input = ({title, state, name}, onChange, index) => {
     return (
         <div key={index + 1} className="input">
-        <label className="input__number">{index + 1}</label>
-            <input value={state} onChange={onChange} name={name}/>
+        <label className={`input__number ${state == '' ? 'gray' : 'green'}`}>{index + 1}</label>
+            <input placeholder={title} value={state} onChange={onChange} name={name}/>
             <label className="input__title">{title}</label>
         </div>
     )
